d786c9f98b8210e415c4d8bc37c2661ce09cce24
mrsho3ib <89498435+mrsho3ib@users.noreply.github.com>
2022-04-09 12:36:09 +0300

    Add files via upload

---
+++ (python-dart-pass)/shoaib_TaskQ1.dart
@@ -0,0 +1,6 @@
+void main() {
+  var hour = DateTime.now().hour;
+  /*change const to var or maybe the final I don't know exactly
+    But they both work*/
+  print(hour);
+}
---
+++ (python-dart-pass)/shoaib_Task_Q2.dart
@@ -0,0 +1,14 @@
+abstract class Animal {
+  void printName() {}
+  void printSound() {}
+}
+
+class Dog extends Animal {}
+
+class Cat extends Animal {}
+
+class Cow extends Animal {}
+
+void main() {
+  print("hi unicoding *_* ");
+}
